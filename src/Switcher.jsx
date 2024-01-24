9cfea67301d2794d8e62221d63ceca58afe8827a
miraj004 <miraj.cei@gmail.com>
2024-01-24 21:35:14 +0430

    Dark Mode Applied!

---
+++ src/Switcher.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+import { ToggleSwitch } from 'flowbite-react';
+import useDarkSide from "./darkmode/useDarkSide.js";
+
+export default function Switcher() {
+    const [colorTheme, setTheme] = useDarkSide();
+    const [darkSide, setDarkSide] = useState(colorTheme === 'light' ? true : false);
+
+    const toggleDarkMode = checked => {
+        setTheme(colorTheme);
+        setDarkSide(checked);
+    };
+
+    return (
+        <>
+            <div>
+                <ToggleSwitch checked={darkSide} onChange={toggleDarkMode} size={56} />
+            </div>
+        </>
+    );
+}
